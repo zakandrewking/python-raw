c34bd57b51e037e237da18ef7fa11bc2be1240fc
Zachary King <zak@delfina.com>
2025-08-28 09:08:09 -0700

    lock

--- scripts/myscript.py
+++ scripts/myscript.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env -S uv run --script
+
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
 #     "polars",
 # ]
 # ///
+
 import polars as pl
 
 print(f"Hello polars version {pl.__version__}")
